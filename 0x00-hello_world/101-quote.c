f90bd8264c03b0fa8cbc2f7553be8bc756276fce
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2025-01-01 13:01:50 +0300

    C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry program that prints a line to the standard error
+ *
+ * Return: 1 Indicates success
+ */
+
+int main(void)
+
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if the program executes successfully
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
